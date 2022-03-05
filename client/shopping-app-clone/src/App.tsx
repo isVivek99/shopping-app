eae0da8b11c002a3f59e9ffed3c1eff6a35fc030
vivek lokhande <vivek2811999@gmail.com>
2022-03-05 16:00:03 +0530

    added navbar common component

--- client/shopping-app-clone/src/App.tsx
+++ client/shopping-app-clone/src/App.tsx
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './assets/scss/App.scss';
+import Homepage from './components/screens/Homepage';
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App '>
+      <Homepage />
+      adda
     </div>
   );
 }
---
+++ client/shopping-app-clone/src/components/common/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InputElementOne from './input/InputElementOne';
+function navbar() {
+  return (
+    <div>
+      navbar
+      <InputElementOne />
+    </div>
+  );
+}
+
+export default navbar;
