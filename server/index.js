cdd6967e20e1af62ba616dc71b47c741ceaeb9d5
vivek lokhande <vivek2811999@gmail.com>
2022-08-17 17:26:20 +0530

    ✨ feat(refresh api): added api

--- server/index.js
+++ server/index.js
@@ -141,6 +141,45 @@ app.get('/api/login', async (req, res) => {
   }
 });
 
+app.post('/api/refreshtoken', async (req, res) => {
+  const { refreshToken: requestToken } = req.body;
+
+  if (requestToken == null) {
+    return res.status(403).json({ message: 'Refresh Token is required!' });
+  }
+
+  try {
+    const refreshToken = await RefreshToken.findOne({ token: requestToken });
+    if (!refreshToken) {
+      res.status(403).json({ message: 'Refresh token is not in database!' });
+      return;
+    }
+    if (RefreshToken.verifyExpiration(refreshToken)) {
+      RefreshToken.findByIdAndRemove(refreshToken._id, {
+        useFindAndModify: false,
+      }).exec();
+
+      res.status(403).json({
+        message: 'Refresh token was expired. Please make a new signin request',
+      });
+      return;
+    }
+    let newAccessToken = jwt.sign(
+      { id: refreshToken.user._id },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: '3600',
+      }
+    );
+    return res.status(200).json({
+      accessToken: newAccessToken,
+      refreshToken: refreshToken.token,
+    });
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
+});
+
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
