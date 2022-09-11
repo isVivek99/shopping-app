b9a46701a64188bb1d9da32d91dd2e49dbc64847
vivek lokhande <vivek2811999@gmail.com>
2022-09-11 15:19:39 +0530

    🐞 fix(revamp-ts-BE):

--- server/config/prod.ts
+++ server/config/prod.ts
@@ -1,7 +1,7 @@
 export module prod {
   export const prodConfig = {
     JWT_SECRET: process.env.JWT_SECRET,
-    PORT: process.env.PORT,
+    PORT: 'https://shopping-app-beryl.vercel.app/',
     MONGODB_URI: process.env.MONGODB_URI,
   };
 }
